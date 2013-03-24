02aea020c1e743b0ab24e3dea42e82cf45a922be
Hidenori Miyoshi <miyohide@gmail.com>
2013-03-24 14:40:51 +0900

    3:5b8 problem solve

--- 02_binary_digit_rotate/lib/binary_digit_rotate.rb
+++ 02_binary_digit_rotate/lib/binary_digit_rotate.rb
@@ -6,7 +6,54 @@ class BinaryDigitRotate
    end
 
    def calc
+      calc_side + ":" + calc_hex
    end
+
+   private
+   def calc_side
+      @binary_digit.split(/:/)[0]
+   end
+
+   def calc_hex
+      rotate(@binary_digit.split(/:/)[1])
+   end
+
+   def rotate(hex_str)
+      digit_str = ''
+      hex_str.each_char do |c|
+         digit_str += "%04d" % c.to_i(16).to_s(2)
+      end
+      side = calc_side.to_i
+      # 一辺の長さの文字ずつ区切る
+      digit_str_list = digit_str.unpack("a#{side}"*(digit_str.size/side))
+
+      digit_str_list = digit_str_list[0..side - 1]
+      # 90度回転。
+      digit_str_list.reverse!
+      digit_str = ''
+      side.times do |i|
+         digit_str_list.each do |element|
+            digit_str += element[i]
+         end
+      end
+
+      # 足りない桁を0で埋める
+      if digit_str.size < 4 * side
+         digit_str += '0' * ( 4 * side - digit_str.size )
+      end
+
+      # 16進変換するために4桁ごとに区切る
+      digit_str_list = digit_str.unpack("a4"*(digit_str.size/4))
+
+      rotate_hex = ''
+      digit_str_list.each do |digit|
+         rotate_hex += digit.to_i(2).to_s(16)
+      end
+
+      rotate_hex
+
+   end
+
 
 end
 
--- 02_binary_digit_rotate/spec/binary_digit_rotate_spec.rb
+++ 02_binary_digit_rotate/spec/binary_digit_rotate_spec.rb
@@ -3,9 +3,18 @@
 require './lib/binary_digit_rotate'
 
 describe BinaryDigitRotate do
-   describe '3:5b8 calc' do
-      subject { BinaryDigitRotate.new('3:5b8').calc }
-      it { should eq '3:de0' }
+   describe '3:5b8' do
+      let(:input_data) { '3:5b8' }
+      describe 'calc_side' do
+         subject { BinaryDigitRotate.new(input_data).send(:calc_side) }
+         it { should eq '3' }
+      end
+
+      describe 'calc' do
+         subject { BinaryDigitRotate.new(input_data).calc }
+         it { should eq '3:de0' }
+      end
    end
+
 end
 
